0d8aafa3be718c0f2faafe2625b70351348c5e4b
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-28 09:16:09 -0600

    fixed import issue

--- src/app/news-list/news-list.component.ts
+++ src/app/news-list/news-list.component.ts
@@ -1,35 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NewsService } from '../news.service';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
-  styleUrls: ['./news-list.component.css']
+  styleUrls: ['./news-list.component.css'],
 })
 export class NewsListComponent implements OnInit {
   @Input() selectedCategory: string = 'business';
   headlines: any;
   // selectedCategory: string = 'business';
 
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
     this.getNews();
   }
 
   getNews() {
-    this.newsService.getTopHeadlines(this.selectedCategory).subscribe((data: any) => {
-      this.headlines = data.articles;
-      console.log(this.headlines);
-    });
+    this.newsService
+      .getTopHeadlines(this.selectedCategory)
+      .subscribe((data: any) => {
+        this.headlines = data.articles;
+        console.log(this.headlines);
+      });
   }
 
   onCategoryChanged(category: string) {
     this.selectedCategory = category;
     this.getNews();
   }
-  
 }
-
-
-

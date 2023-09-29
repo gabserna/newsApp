a93cf2a33f6cd2562bbde919cb37ea9ba10f89d0
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-29 09:57:40 -0600

    merge with main

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { NewsListComponent } from '../components/news-list/news-list.component';
+import { RegistrationComponent } from '../components/registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
   { path: 'top-headlines', component: NewsListComponent },
 ];
 

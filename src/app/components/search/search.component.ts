bc58f8a6b8a94825b26b0d2c61d790fff87d0158
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-10-01 21:58:27 -0600

    .

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NewsService } from "../../services/news.service";
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit{
+  @Input() searchFilter: string = 'busisness';
+  headlines:any
+
+  constructor( private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.getSearchResults()
+  }
+
+  getSearchResults() {
+
+    this.newsService
+      .searchArticles(this.searchFilter)
+      .subscribe((data: any) => {
+        this.headlines = data.articles;
+        console.log(this.headlines);
+      })
+
+  }
+
+  onFilterChange(filter: string) {
+    this.searchFilter = filter;
+    this.getSearchResults()
+  }
+
+}

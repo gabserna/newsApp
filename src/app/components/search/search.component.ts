290cea7da6e36d2af8ff0f7f95e45b9be507115b
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-10-02 18:39:11 -0600

    Populates search page

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NewsService } from "../../services/news.service";
 
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit{
-  @Input() searchFilter: string = 'busisness';
-  headlines:any
+  @Input() searchFilter: string = 'apple';
+  headlines: any;
+
 
   constructor( private newsService: NewsService) {}
 

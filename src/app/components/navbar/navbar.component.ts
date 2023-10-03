298432117dc8ab46bcebb216646c5d76d4653dcb
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-10-03 00:51:33 -0600

    headlines, card and buttons styled

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, HostListener } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
@@ -7,8 +7,29 @@ import { MatButtonToggleChange } from '@angular/material/button-toggle';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  windowInnerWidth: number;
+
+  constructor() {
+    this.windowInnerWidth = window.innerWidth;
+  }
+
+  categories = [
+    { name: 'General', icon: 'description' },
+    { name: 'Business', icon: 'add_business' },
+    { name: 'Entertainment', icon: 'theater_comedy' },
+    { name: 'Health', icon: 'health_and_safety' },
+    { name: 'Science', icon: 'biotech' },
+    { name: 'Sports', icon: 'sports_football' },
+    { name: 'Technology', icon: 'important_devices' },
+  ];
+
   selectedCategory: string = 'general';
   @Output() categoryChanged = new EventEmitter<string>();
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowInnerWidth = window.innerWidth;
+  }
 
   onCategoryChange(event: MatButtonToggleChange) {
     this.selectedCategory = event.value;

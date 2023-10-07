edf666535f210348a9b84336d38256389cf3cfa6
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-10-07 17:47:10 -0600

    toolbar date, time added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currentDay: string = '';
+  currentDate: Date = new Date();
+  currentTime: Date = new Date();
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.updateTime();
+    setInterval(() => this.updateTime(), 1000);
+  }
+
+  private updateTime() {
+    const now = new Date();
+    this.currentDay = this.getDayOfWeek(now.getDay());
+    this.currentDate = now;
+    this.currentTime = now;
+  }
+
+  private getDayOfWeek(dayIndex: number): string {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return daysOfWeek[dayIndex];
+  }
 }
